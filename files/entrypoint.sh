c02f7fee61c7234676fe1a3e6e8500661a67e2ab
Daniel Hawton <daniel@hawton.com>
2021-03-21 22:33:35 -0500

    entrypoint change

--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -3,8 +3,11 @@ set -e
 
 cd /var/www/html
 
-echo 'Migrating database...'
-php artisan migrate
+# Only migrate if installed
+if [[ -f /var/www/html/.installed ]]; then
+  echo 'Migrating database...'
+  php artisan migrate
+fi
 
 echo 'Verifying permissions'
 chown -R www-data:www-data /var/www/html
